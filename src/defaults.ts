806d05927a33e134ffcc0c93d2c3137d9624130c
Mohammad Javad Khademian <mjkhonline@live.com>
2023-09-14 22:08:54 +0330

    define defaults

---
+++ src/defaults.ts
@@ -0,0 +1,21 @@
+import { QueryOptions, RetryFn, RetryDelayFn } from "./index.d"
+
+export const defaultQueryRetryFn: RetryFn = (retries: number): boolean => {
+    return retries < 3
+}
+
+export const defaultQueryRetryDelay: RetryDelayFn = (retries: number): number => {
+    return Math.min(500 * 2 ** retries, 60000)
+}
+
+export const defaultQueryOptions: QueryOptions = {
+    staleTime: 0,
+    force: false,
+    hardForce: false,
+    retry: defaultQueryRetryFn,
+    retryDelay: defaultQueryRetryDelay,
+    distinctRetry: false,
+    deactivate: false,
+    deactivateOnWindowHidden: false,
+    refetchOnWindowFocus: false
+}
